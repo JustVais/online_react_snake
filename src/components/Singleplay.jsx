051e278766c6913cb07d7acdbcdb947cd1b81627
JustVais <justvais@mail.ru>
2020-03-25 17:37:30 +0500

    single player game is ready

--- src/components/Singleplay.jsx
+++ src/components/Singleplay.jsx
@@ -5,6 +5,13 @@ import '../css/singleplay.css'
 const MAP_SIZE = 16;
 const MAP = [];
 
+const Direction = {
+    Top: { x: 0, y: -1 },
+    Bottom: { x: 0, y: 1 },
+    Left: { x: -1, y: 0 },
+    Right: { x: 1, y: 0 }
+};
+
 class Singleplay extends Component {
 
     constructor() {
@@ -14,10 +21,14 @@ class Singleplay extends Component {
 
         this.state = {
             currentSnake: [
-                { x: 8, y: 8 },
+                { x: 8, y: 6 },
                 { x: 8, y: 7 },
-                { x: 8, y: 6 }
-            ]
+                { x: 8, y: 8 },
+            ],
+
+            currentApple: this.spawnApple(),
+
+            currentDirrection: Direction.Left
         };
     }
 
@@ -27,18 +38,107 @@ class Singleplay extends Component {
         }
     }
 
-    checkCell = (x, y, cell) => {
+
+    checkCell = (x, y) => {
+
+        let { currentSnake, currentApple } = this.state;
 
         let result_class = "";
 
-        this.state.currentSnake.forEach(piece_of_snake => {
+        currentSnake.forEach(piece_of_snake => {
+
             if (piece_of_snake.x === x && piece_of_snake.y === y) {
+
                 result_class = "map__snake";
-                return result_class;
+
+            } else if (currentApple.x === x && currentApple.y === y) {
+
+                result_class = "map__apple";
+
             }
+
         });
 
         return result_class;
+    }
+
+    eatApple(head) {
+
+        let { currentApple } = this.state;
+
+        if (head.x === currentApple.x && head.y === currentApple.y) {
+            this.setState({
+                currentApple: this.spawnApple()
+            });
+            return true;
+        }
+        return false;
+    }
+
+    isInTheField(head) {
+        // console.log(head.x, head.y);
+        if (head.x < 0 || head.x > 15  
+            || head.y < 0 || head.y > 15) {
+            return false;
+        }
+        return true;
+    }
+
+    moveSnake() {
+        let { currentSnake, currentDirrection } = this.state;
+
+        let [head] = currentSnake;
+        let tail = [];
+
+        let newHead = {
+            x: head.x + currentDirrection.x,
+            y: head.y + currentDirrection.y,
+        };
+
+        if (!this.isInTheField(newHead)) {
+            clearInterval(this.interval);
+            window.removeEventListener('keydown', () => {});
+            alert('Game Over!');
+            return;
+        }
+
+        tail = this.eatApple(newHead) ? currentSnake.slice(0) : currentSnake.slice(0, -1);
+
+        this.setState({
+            currentSnake: [newHead, ...tail]
+        });
+    }
+
+    spawnApple() {
+        let newApple = { x: 0, y: 0 }
+
+        newApple.x = Math.floor(Math.random() * MAP_SIZE);
+        newApple.y = Math.floor(Math.random() * MAP_SIZE);
+
+        return newApple;
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.moveSnake();
+        }, 250);
+
+        window.addEventListener('keydown', (event) => {
+            if (event.code == 'KeyW') {
+                this.state.currentDirrection = Direction.Top;
+            } else if (event.code == 'KeyS') {
+                this.state.currentDirrection = Direction.Bottom;
+            } else if (event.code == 'KeyA') {
+                this.state.currentDirrection = Direction.Left;
+            } else if (event.code == 'KeyD') {
+                this.state.currentDirrection = Direction.Right;
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+        window.removeEventListener('keydown', () => {});
     }
 
     render() {
@@ -48,8 +148,8 @@ class Singleplay extends Component {
                     MAP.map((row, y) =>
                         <div key={y} className="map__row">
                             {
-                                row.map((cell, x) => 
-                                    <div key={x} className={`map__cell ${this.checkCell(x, y, cell)}`}></div>   
+                                row.map((cell, x) =>
+                                    <div key={x} className={`map__cell ${this.checkCell(x, y, cell)}`}></div>
                                 )
                             }
                         </div>
